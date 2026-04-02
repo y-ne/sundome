2c559c9e2cfd81e2e508ec6f204b34ea1d801479
Millimeter <akumakumu@gmail.com>
2026-04-02 15:49:03 +0700

    part 2 of tutorial, adding KeyVal model and register it to admin page

--- core/models.py
+++ core/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class KeyVal(models.Model):
+    key = models.CharField(max_length=255)
+    value = models.TextField()
+
+    def __str__(self) -> str:
+        return str(self.key)
